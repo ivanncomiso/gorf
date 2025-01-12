cc1dd3d4e3533b0c2a18b8ed42deaa665766f298
ivanncomiso <princeivannbcomiso@gmail.com>
2025-01-13 00:15:33 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,16 @@
-import React, { useState } from 'react';
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import Footer from './components/Footer';
+import { useState } from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Footer from './components/Footer'
 
 function App() {
-  React 
-  // Useless state to satisfy the unused import warning
-  const [, setUselessState] = useState(false);
-
-  // Useless function to "use" the state
-  const uselessFunction = () => {
-    setUselessState((prev) => !prev);
-  };
-
-  // Call the useless function (but it won't affect anything)
-  uselessFunction();
-
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
       <Hero />
       <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
