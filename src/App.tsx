ed19da5a1a7eb9c8c641b6778c35db60a2637e6d
Ivann Comiso <ivannc@Ivanns-MacBook-Pro.local>
2025-01-12 23:36:24 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      <Hero />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
